fc8b0245d2ae9641b18a181185f5fb5a857285cb
filipecosta90 <filipecosta.90@gmail.com>
2020-07-20 17:41:08 +0100

    [fix] fixes per PR review

--- example_client_test.go
+++ example_client_test.go
@@ -47,11 +47,11 @@ func ExampleMultiRangeWithOptions() {
 	}}
 	client := redistimeseries.NewClientFromPool(pool, "ts-client-1")
 
-	labels := map[string]string{
+	labels1 := map[string]string{
 		"machine": "machine-1",
 		"az":      "us-east-1",
 	}
-	client.AddWithOptions("time-serie-1", 2, 1.0, redistimeseries.CreateOptions{RetentionMSecs: 0, Labels: labels})
+	client.AddWithOptions("time-serie-1", 2, 1.0, redistimeseries.CreateOptions{RetentionMSecs: 0, Labels: labels1})
 	client.Add("time-serie-1", 4, 2.0)
 
 	labels2 := map[string]string{
@@ -59,13 +59,13 @@ func ExampleMultiRangeWithOptions() {
 		"az":      "us-east-1",
 	}
 	client.AddWithOptions("time-serie-2", 1, 5.0, redistimeseries.CreateOptions{RetentionMSecs: 0, Labels: labels2})
-	client.Add("time-serie-1", 4, 2.0)
+	client.Add("time-serie-2", 4, 10.0)
 
 	ranges, _ := client.MultiRangeWithOptions(1, 10, redistimeseries.DefaultMultiRangeOptions, "az=us-east-1")
 
 	fmt.Println(fmt.Sprintf("Ranges: %v", ranges))
 	// Output:
-	// Ranges: [{time-serie-1 map[] [{2 1} {4 2}]} {time-serie-2 map[] [{1 5}]}]
+	// Ranges: [{time-serie-1 map[] [{2 1} {4 2}]} {time-serie-2 map[] [{1 5} {4 10}]}]
 }
 
 // Exemplifies the usage of MultiGetWithOptions function while using the default MultiGetOptions and while using user defined MultiGetOptions.
@@ -77,11 +77,11 @@ func ExampleMultiGetWithOptions() {
 	}}
 	client := redistimeseries.NewClientFromPool(pool, "ts-client-1")
 
-	labels := map[string]string{
+	labels1 := map[string]string{
 		"machine": "machine-1",
 		"az":      "us-east-1",
 	}
-	client.AddWithOptions("time-serie-1", 2, 1.0, redistimeseries.CreateOptions{RetentionMSecs: 0, Labels: labels})
+	client.AddWithOptions("time-serie-1", 2, 1.0, redistimeseries.CreateOptions{RetentionMSecs: 0, Labels: labels1})
 	client.Add("time-serie-1", 4, 2.0)
 
 	labels2 := map[string]string{
@@ -89,7 +89,7 @@ func ExampleMultiGetWithOptions() {
 		"az":      "us-east-1",
 	}
 	client.AddWithOptions("time-serie-2", 1, 5.0, redistimeseries.CreateOptions{RetentionMSecs: 0, Labels: labels2})
-	client.Add("time-serie-1", 4, 2.0)
+	client.Add("time-serie-2", 4, 10.0)
 
 	ranges, _ := client.MultiGetWithOptions(redistimeseries.DefaultMultiGetOptions, "az=us-east-1")
 
@@ -99,6 +99,6 @@ func ExampleMultiGetWithOptions() {
 	fmt.Println(fmt.Sprintf("Ranges with labels: %v", rangesWithLabels))
 
 	// Output:
-	// Ranges: [{time-serie-1 map[] [{4 2}]} {time-serie-2 map[] [{1 5}]}]
-	// Ranges with labels: [{time-serie-1 map[az:us-east-1 machine:machine-1] [{4 2}]} {time-serie-2 map[az:us-east-1 machine:machine-2] [{1 5}]}]
+	// Ranges: [{time-serie-1 map[] [{4 2}]} {time-serie-2 map[] [{4 10}]}]
+	// Ranges with labels: [{time-serie-1 map[az:us-east-1 machine:machine-1] [{4 2}]} {time-serie-2 map[az:us-east-1 machine:machine-2] [{4 10}]}]
 }
